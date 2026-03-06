d1007a29e40588c4b05d45ea526abccb549e0217
“NicoSimo” <nicolo.simonato22@gmail.com>
2026-03-06 12:59:28 +0100

    adding libero-pro benchmark

--- src/lerobot/envs/factory.py
+++ src/lerobot/envs/factory.py
@@ -20,7 +20,7 @@ import gymnasium as gym
 from gymnasium.envs.registration import registry as gym_registry
 
 from lerobot.configs.policies import PreTrainedConfig
-from lerobot.envs.configs import AlohaEnv, EnvConfig, HubEnvConfig, IsaaclabArenaEnv, LiberoEnv, PushtEnv
+from lerobot.envs.configs import AlohaEnv, EnvConfig, HubEnvConfig, IsaaclabArenaEnv, LiberoEnv, PushtEnv, LiberoProEnv
 from lerobot.envs.utils import _call_make_env, _download_hub_file, _import_hub_module, _normalize_hub_result
 from lerobot.policies.xvla.configuration_xvla import XVLAConfig
 from lerobot.processor import ProcessorStep
@@ -35,6 +35,8 @@ def make_env_config(env_type: str, **kwargs) -> EnvConfig:
         return PushtEnv(**kwargs)
     elif env_type == "libero":
         return LiberoEnv(**kwargs)
+    elif env_type == "libero-pro":
+        return LiberoProEnv(**kwargs)
     else:
         raise ValueError(f"Policy type '{env_type}' is not available.")
 
@@ -165,7 +167,23 @@ def make_env(
 
     env_cls = gym.vector.AsyncVectorEnv if use_async_envs else gym.vector.SyncVectorEnv
 
-    if "libero" in cfg.type:
+    if "libero-pro" in cfg.type:
+        from lerobot.envs.liberopro import create_libero_pro_envs
+
+        if cfg.task is None:
+            raise ValueError("LiberoEnv requires a task to be specified")
+
+        return create_libero_pro_envs(
+            task=cfg.task,
+            n_envs=n_envs,
+            camera_name=cfg.camera_name,
+            init_states=cfg.init_states,
+            gym_kwargs=cfg.gym_kwargs,
+            env_cls=env_cls,
+            control_mode=cfg.control_mode,
+            episode_length=cfg.episode_length,
+        )
+    elif "libero" in cfg.type:
         from lerobot.envs.libero import create_libero_envs
 
         if cfg.task is None:
